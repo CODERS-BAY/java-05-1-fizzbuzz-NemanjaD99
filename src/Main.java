162632010cd2dd8e2865ef4142b5ca0f6d79a03d
Nemanja <nemanjadragomirovic086@gmail.com>
2021-03-01 08:15:15 +0100

    Implement Java 5.1. FizzBuzz

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,11 @@ public class Main {
 
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if ( i % 5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
-            else   {System.out.println(i);}
         }
 
 
